864fd33d1c5e43baa09ab816d89ff3702c882c39
Eric Krouse <erickrousemusic@gmail.com>
2021-06-30 11:13:33 -0400

    Models and constructors and getters/setters

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,42 @@
+package com.techelevator.tenmo.model;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class Account {
+    private int accountId;
+    @NotNull
+    private int userId;
+    @Min(value = 0)
+    private double balance;
+
+    public Account (int accountId, int userId, double balance) {
+        this.accountId = accountId;
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
