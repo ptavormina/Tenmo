271639f3789e2e6ddb7c5ae48784efd7dc9aae34
Paige Tavormina <ptavormina21@gmail.com>
2021-07-02 11:34:01 -0400

    merge

--- tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -16,8 +16,6 @@ public class Transfer {
     private String userTo;
 
     public Transfer(int transferId, int statusId, int typeId, int accountFrom, int accountTo, BigDecimal transferAmount, String transferType, String transferStatus, String userFrom, String userTo) {
-
-    public Transfer(int transferId, int statusId, int typeId, int accountFrom, int accountTo, BigDecimal transferAmount) {
         this.transferId = transferId;
         this.statusId = statusId;
         this.typeId = typeId;
@@ -28,6 +26,14 @@ public class Transfer {
         this.transferStatus = transferStatus;
         this.userFrom = userFrom;
         this.userTo = userTo;
+    }
+    public Transfer(int transferId, int statusId, int typeId, int accountFrom, int accountTo, BigDecimal transferAmount) {
+        this.transferId = transferId;
+        this.statusId = statusId;
+        this.typeId = typeId;
+        this.accountFrom = accountFrom;
+        this.accountTo = accountTo;
+        this.transferAmount = transferAmount;
     }
 
     public Transfer(){
@@ -111,13 +117,5 @@ public class Transfer {
 
     public void setAccountTo(int accountTo) {
         this.accountTo = accountTo;
-    }
-
-    public BigDecimal getTransferAmount() {
-        return transferAmount;
-    }
-
-    public void setTransferAmount(BigDecimal transferAmount) {
-        this.transferAmount = transferAmount;
     }
 }
